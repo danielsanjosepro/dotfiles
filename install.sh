0b62f981becd2b6813a8b3f4f5c8df159c49bd2a
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2024-04-04 10:27:57 +0200

    Changed completly the structure of the dotfiles using symlinks

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+# List of folders to simlink
+ln -s ~/.dotfiles/config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/config/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/config/waybar ~/.config/waybar
+ln -s ~/.dotfiles/config/darktable ~/.config/darktable
+ln -s ~/.dotfiles/config/wal ~/.config/wal
+ln -s ~/.dotfiles/config/vis ~/.config/vis
+ln -s ~/.dotfiles/config/dunst ~/.config/dunst
+ln -s ~/.dotfiles/config/hypr ~/.config/hypr
+
+# List of files to simlink
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/.p10k_simple.zsh ~/.p10k_simple.zsh
+ln -s ~/.dotfiles/.p10k_complex.zsh ~/.p10k_complex.zsh
+ln -s ~/.dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
--- util/copy_gnome_extensions.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Set the source and destination paths
-source="$HOME/.local/share/gnome-shell/extensions"
-destination="$HOME/git/dotfiles/configs/gnome-shell/extensions"
-
-# Copy the extensions to the destination directory
-cp -r "$source" "$destination"
