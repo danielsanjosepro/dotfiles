b7c078be6e35b748b860ec78a8e91044a5d59f9a
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-04-20 11:07:39 +0200

    Found a better solution to jump between files with marks

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -400,6 +400,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+			vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "[F]ind [M]arks" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
