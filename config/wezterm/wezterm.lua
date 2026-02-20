39e4cec8a15b84b372b2da78570ea6734bffca37
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2026-02-20 11:24:38 +0100

    wezterm small changes for wayland

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,14 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrains Mono")
+local font = wezterm.font_with_fallback({
+	{ family = "Hack Nerd Font", style = "Normal", scale = 1 },
+	{ family = "Cambria Math", scale = 1.0 },
+})
 config.font_size = 18.0
 
 config.enable_tab_bar = true
+config.enable_wayland = false
 config.use_fancy_tab_bar = true
 
 config.window_decorations = "RESIZE"
